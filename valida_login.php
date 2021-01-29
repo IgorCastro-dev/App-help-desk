0a4e2afc9e31cf9c61399075157f9b690fb9e714
IgorCastro-dev <igorjunqueira19@gmail.com>
2021-01-29 10:30:48 -0300

    :package: navegação
    fazendo a navegação entre as páginas

--- valida_login.php
+++ valida_login.php
@@ -9,6 +9,7 @@ foreach($usuarios as $user){
 }
     if($usuario_autenticado){
         $_SESSION['autenticado'] = 'SIM';
+        header('Location: home.php');
     }else{
         $_SESSION['autenticado'] = 'NAO';
         header('Location: index.php?login=erro');
